51e0cef5db5d2df9c4a53948efc1ad2c7491c4bd
Feral-Jas <906028518@qq.com>
2019-12-06 10:30:48 +0800

    edited 1206

--- ArrayListDisplay.java
+++ ArrayListDisplay.java
@@ -14,21 +14,44 @@ public class ArrayListDisplay {
 		arraylist.add(5);
 		arraylist.add(4);
 		arraylist.add(3);
-		/* foreach API */
-		System.out.println("forEach API");
+		/* APIs */
+		System.out.println("forEach");
 		arraylist.forEach(item -> System.out.print(item + " "));
-		System.out.print('\n' + "stream + filter API + forEach API" + '\n');
-		arraylist.stream().filter(item -> item != null).forEach(item -> System.out.print(item + " "));
-		System.out.print('\n' + "stream + sort API + forEach" + '\n');
-		arraylist.stream().sorted().forEach(item -> System.out.print(item + " "));
-		System.out.print('\n' + "stream + sort API(Comparator) + forEach" + '\n');
-		arraylist.stream().sorted(
-				(before,after)->{
-					if(before>after)
-						return -1;
-					else
-						return 1;
+                System.out.println();
+
+		System.out.print('\n' + "stream + filter  + forEach " + '\n');
+		arraylist.stream()
+                	 .filter(item -> item != null)
+                         .forEach(item -> System.out.print(item + " "));
+
+                System.out.println();
+		System.out.print('\n' + "stream + sorted + forEach" + '\n');
+		arraylist.stream()
+                         .sorted()
+                         .forEach(item -> System.out.print(item + " "));
+
+                System.out.println();
+		System.out.print('\n'+"stream + sorted + filter(3) + forEach" + '\n');
+		arraylist.stream()
+                         .sorted()
+                         .filter(item->item!=3)
+                         .forEach(item -> System.out.print(item + " "));
+
+                System.out.println();
+		System.out.print('\n'+"stream + sorted(Comparator) + forEach" + '\n');
+		arraylist.stream()
+                         .sorted((before,after)->{
+				if(before>after)
+					return -1;
+				else
+					return 1;
 				}
-				).forEach(item -> System.out.print(item + " "));
+		         )
+                         .forEach(item -> System.out.print(item + " "));
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+
 	}
 }
